a7d362978de231aea3c3a781b5f3ceb125ea3026
Rob Bartholomew <rob@racm.ca>
2023-04-18 16:12:27 -0400

    Chart values accordian

--- calculateAccordians.js
+++ calculateAccordians.js
@@ -47,3 +47,6 @@ $('#accordian-parameters').on('click', function(){
     $('#content-spouse').animate({height: 'toggle'}, 500);
  });
  
+ $('#accordian-chartvalues').on('click', function(){
+   $('#content-chartvalues').animate({height: 'toggle'}, 500);
+});
