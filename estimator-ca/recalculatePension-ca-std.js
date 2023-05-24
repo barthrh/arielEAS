51582e8775936e74146ad1f7a6994e3e2d678547
Rob Bartholomew <rob@racm.ca>
2023-05-24 12:02:05 -0400

    Still more legend-purchase typos

--- estimator-ca/recalculatePension-ca-std.js
+++ estimator-ca/recalculatePension-ca-std.js
@@ -14,9 +14,9 @@ function recalculatePension() {
     if ($('#check-purchase').prop("checked") == true &&
         $('#check-reinstated-2yrs').prop("checked") == true) {
         purchaseYears = 2;
-        $('legend-purchase').animate({height: 'show'}, 0);
+        $('#legend-purchase').animate({height: 'show'}, 0);
     } else {
-        $('legend-purchase').animate({height: 'hide'}, 0);  
+        $('#legend-purchase').animate({height: 'hide'}, 0);  
     };
 
     govpenYears = 4;  
