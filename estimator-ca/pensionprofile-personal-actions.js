774f83f9183e17d257113056a3a60963fec077d9
Rob Bartholomew <rob@racm.ca>
2023-06-06 12:01:36 -0400

    Expanding show/hide to more items

--- estimator-ca/pensionprofile-personal-actions.js
+++ estimator-ca/pensionprofile-personal-actions.js
@@ -8,6 +8,14 @@ $('#nav-basic-tab').on('click', function(){
 
 $('#nav-survivor-tab').on('click', function(){
     setBasic('hide');
+    setSurvivor('show');
+    setAuthrep('hide');
+})
+
+$('#nav-authrep-tab').on('click', function(){
+    setBasic('hide');
+    setSurvivor('hide');
+    setAuthrep('show');
 })
 
 function setBasic(theAction) {
@@ -20,11 +28,34 @@ function setBasic(theAction) {
         var showHide = 'hide';
     }
     $('#nav-basic-text').css('color',textColor);
-    $('#nav-basic-arrow').animate({height: showHide}, 500);
-    $('#nav-basic-content').animate({height: showHide}, 500);
+    $('#nav-basic-arrow').animate({height: showHide}, 0);
+    $('#nav-basic-content').animate({height: showHide}, 300);
 }
 
-function setAuthrep() {
-    //nothing
+function setSurvivor(theAction) {
+    
+    if (theAction == 'show') {
+        var textColor = '#0078AB';
+        var showHide = 'show';
+    } else {
+        var textColor = '#595959';
+        var showHide = 'hide';
+    }
+    $('#nav-survivor-text').css('color',textColor);
+    $('#nav-survivor-arrow').animate({height: showHide}, 0);
+    $('#nav-survivor-content').animate({height: showHide}, 300);
+}
+
+function setAuthrep(theAction) {
+    if (theAction == 'show') {
+        var textColor = '#0078AB';
+        var showHide = 'show';
+    } else {
+        var textColor = '#595959';
+        var showHide = 'hide';
+    }
+    $('#nav-authrep-text').css('color',textColor);
+    $('#nav-authrep-arrow').animate({height: showHide}, 0);
+    $('#nav-authrep-content').animate({height: showHide}, 300);
 }
 // nav-authrep-tab
