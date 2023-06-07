2f44f91466f0c30939f7228e587807c7e882533e
Rob Bartholomew <rob@racm.ca>
2023-06-07 17:00:51 -0400

    New setup actions to initialize section visibility

---
+++ estimator-ca/pensionprofile-membership-setup.js
@@ -0,0 +1,6 @@
+// One-time actions at page load
+
+$('#content-retirementdates').animate({height: 'show'}, 500);
+$('#content-service').animate({height: 'hide'}, 500);
+$('#content-contributions').animate({height: 'hide'}, 500);
+$('#content-salaries').animate({height: 'hide'}, 500);
