24a661bad4dcf3a57c8e6dd5a3e897d31e6b6b23
Rob Bartholomew <rob@racm.ca>
2023-06-09 10:47:48 -0400

    Missing semicolon

--- estimator-ca/memberportal-home-actions.js
+++ estimator-ca/memberportal-home-actions.js
@@ -3,13 +3,12 @@
 
 $('#button-article-abouttheplan').on('click', function(){
     $('#tile-abouttheplan').animate({width: '100vw'},300);
-    $('#tilesmall-abouttheplan').animate('hide',300);
-//    $('#tilesmall-abouttheplan').animate('hide',{duration: 300, queue: false});
+    $('#tilesmall-abouttheplan').animate('hide',{duration: 300, queue: false});
     $('#tilelarge-abouttheplan').animate({width: '100%'},300);
-})
+});
 
 $('#button-shrink-abouttheplan').on('click', function(){
     $('#tilelarge-abouttheplan').animate('hide',300);
     $('#tile-abouttheplan').animate({width: '250px'},300);
     $('#tilesmall-abouttheplan').animate('show',{duration: 300, queue: false});
-})
+});
