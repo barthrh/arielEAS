21035d30c1cb17db73a5adadd29947a8ec4a4f83
Rob Bartholomew <rob@racm.ca>
2023-06-09 11:20:52 -0400

    Final fix?

--- estimator-ca/memberportal-home-actions.js
+++ estimator-ca/memberportal-home-actions.js
@@ -2,13 +2,13 @@
 $('#button-article-abouttheplan').on('click', function() {
     console.log('pong!');
     $('#tile-abouttheplan').animate({width: '100vw'},300);
-    $('#tilesmall-abouttheplan').animate(width: 'hide',{duration: 300, queue: false});
+    $('#tilesmall-abouttheplan').animate({width: 'hide'},{duration: 300, queue: false});
     $('#tilelarge-abouttheplan').animate({width: '100%'},300);
 });
 
 $('#button-shrink-abouttheplan').on('click', function() {
     console.log('ping!');
-    $('#tilelarge-abouttheplan').animate(width: 'hide',300);
+    $('#tilelarge-abouttheplan').animate({width: 'hide'},300);
     $('#tile-abouttheplan').animate({width: '250px'},300);
-    $('#tilesmall-abouttheplan').animate(width: 'show',{duration: 300, queue: false});
+    $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 300, queue: false});
 });
