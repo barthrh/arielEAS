325fc24530b6e6725489cf4adadd759dab77c459
Rob Bartholomew <rob@racm.ca>
2023-06-09 14:41:09 -0400

    tweaks

--- estimator-ca/memberportal-home-actions.js
+++ estimator-ca/memberportal-home-actions.js
@@ -2,11 +2,11 @@
 $('#button-article-abouttheplan').on('click', function() {
     $('#tile-abouttheplan').animate({width: '100vw'},300);
     $('#tilesmall-abouttheplan').animate({width: 'hide'},{duration: 300, queue: true});
-    $('#tilelarge-abouttheplan').animate({height: 'show'},300);
+    $('#tilelarge-abouttheplan').animate({opacity: 'show'},300);
 });
 
 $('#button-shrink-abouttheplan').on('click', function() {
-    $('#tilelarge-abouttheplan').animate({width: 'hide'},300);
     $('#tile-abouttheplan').animate({width: '250px'},300);
-    $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 300, queue: false});
+    $('#tilelarge-abouttheplan').animate({opacity: 'hide'},0);
+    $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 0, queue: false});
 });
