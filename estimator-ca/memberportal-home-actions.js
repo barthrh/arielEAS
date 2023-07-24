9ed2c2b03b9c24c93f8a3ea929e8fc005a777dc4
Rob Bartholomew <rob@racm.ca>
2023-07-24 15:55:00 -0400

    Dealth with min-width for multiple tiles in row

--- estimator-ca/memberportal-home-actions.js
+++ estimator-ca/memberportal-home-actions.js
@@ -1,12 +1,14 @@
 
 $('#button-article-abouttheplan').on('click', function() {
     $('#tile-abouttheplan').animate({width: '100vw'},300);
+    $('#tile-abouttheplan').css('min-width','700px');
     $('#tilelarge-abouttheplan').animate({opacity: 'show'},300);
     $('#tilesmall-abouttheplan').animate({width: 'hide'},{duration: 0, queue: false});
 });
 
 $('#button-shrink-abouttheplan').on('click', function() {
     $('#tile-abouttheplan').animate({width: '250px'},300);
+    $('#tile-abouttheplan').css('min-width','250px');
     $('#tilelarge-abouttheplan').animate({opacity: 'hide'},0);
     $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 0, queue: false});
 });
--- estimator-ca/memberportal-home-setup.js
+++ estimator-ca/memberportal-home-setup.js
@@ -1,6 +1,7 @@
 
 $( document ).ready(function() {
     $('#tile-abouttheplan').animate({width: '250px'},0);
+    $('#tile-abouttheplan').css('min-width','250px');
     $('#tilelarge-abouttheplan').animate({opacity: 'hide'},0);
     $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 0, queue: false});
     console.log( "ready!" );
