6e78d04da2622ecb08f4f9b0db79a70c993d1320
Rob Bartholomew <rob@racm.ca>
2023-09-04 11:58:57 -0400

    Removed script tag

--- initate-retirement/initretire-endofemploy-setup.js
+++ initate-retirement/initretire-endofemploy-setup.js
@@ -1,22 +1,20 @@
-<script>
-  
-  var endDate = localStorage.getItem('initretire-enddate');
-  var retireDate = localStorage.getItem('initretire-retiredate');
-   
-  // 1. Find the form
-  var form = $('form[data-name="endemploy-form"]'); 
-  
-  if ( localStorage.getItem('initretire-enddate') != null ) {
-     form.find($("#endemploy-date-input-field")).val(initretire-enddate)  
-  } else {
-     form.find($("#endemploy-date-input-field")).val("09-20-2023")  
-  }
-  
-  if ( localStorage.getItem('initretire-retiredate') != null ) {
-     form.find($("#retirement-date")).val(initretire-retiredate)  
-  } else {
-     form.find($("#retirement-date")).val("October 1, 2023")  
-  }
+// Set up the dates and default them if not pre-loaded and stored.
 
-  
-</script>
+var endDate = localStorage.getItem('initretire-enddate');
+var retireDate = localStorage.getItem('initretire-retiredate');
+
+// 1. Find the form
+var form = $('form[data-name="endemploy-form"]'); 
+
+if ( localStorage.getItem('initretire-enddate') != null ) {
+   form.find($("#endemploy-date-input-field")).val(initretire-enddate)  
+} else {
+   form.find($("#endemploy-date-input-field")).val("09-20-2023")  
+}
+
+if ( localStorage.getItem('initretire-retiredate') != null ) {
+   form.find($("#retirement-date")).val(initretire-retiredate)  
+} else {
+   form.find($("#retirement-date")).val("October 1, 2023")  
+}
+
