ef0f0b53f9d469ed083d42fb599db3f4b285755c
Rob Bartholomew <rob@racm.ca>
2023-09-04 15:46:57 -0400

    debug

--- initate-retirement/initretire-endofemploy-setup.js
+++ initate-retirement/initretire-endofemploy-setup.js
@@ -3,20 +3,23 @@
 var endDate = localStorage.getItem('initretire-enddate');
 var retireDate = localStorage.getItem('initretire-retiredate');
 
-console.log("running");
 
 // 1. Find the form
 var form = $('form[data-name="endemploy-form"]'); 
 
 if ( localStorage.getItem('initretire-enddate') != null ) {
    form.find($("#endemploy-date-input-field")).val(endDate)  
+   console.log("Point1");
 } else {
    form.find($("#endemploy-date-input-field")).val("09-20-2023")  
+   console.log("Point2");
 }
 
 if ( localStorage.getItem('initretire-retiredate') != null ) {
    form.find($("#retirement-date")).val(retireDate)  
+   console.log("Point3");
 } else {
    form.find($("#retirement-date")).val("October 1, 2023")  
+   console.log("Point4");
 }
 
