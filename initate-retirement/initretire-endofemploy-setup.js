249e35b6f8416e258a0265c5997d94be36f200b7
Rob Bartholomew <rob@racm.ca>
2023-09-04 11:46:52 -0400

    First scripts for retire initiate

---
+++ initate-retirement/initretire-endofemploy-setup.js
@@ -0,0 +1,22 @@
+<script>
+  
+  var endDate = localStorage.getItem('initretire-enddate');
+  var retireDate = localStorage.getItem('initretire-retiredate');
+   
+  // 1. Find the form
+  var form = $('form[data-name="endemploy-form"]'); 
+  
+  if ( localStorage.getItem('initretire-enddate') != null ) {
+     form.find($("#endemploy-date-input-field")).val(initretire-enddate)  
+  } else {
+     form.find($("#endemploy-date-input-field")).val("09-20-2023")  
+  }
+  
+  if ( localStorage.getItem('initretire-retiredate') != null ) {
+     form.find($("#retirement-date")).val(initretire-retiredate)  
+  } else {
+     form.find($("#retirement-date")).val("October 1, 2023")  
+  }
+
+  
+</script>
