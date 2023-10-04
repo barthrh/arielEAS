12ded4162df9610a3414a6cb4cbc9edda7830d28
Rob Bartholomew <rob@racm.ca>
2023-10-04 11:16:52 -0400

    Initial commit of all of the scripts to support an independent integrated EAS version.

---
+++ arieleas-integrated/estimator-setup-intUS.js
@@ -0,0 +1,14 @@
+console.log("Estimator setup loaded.");
+
+// This is where we queue up all of the initial sizes of the varios boxes and accordians.
+
+$('#content-parameters').animate({height: 'show'}, 0);
+$('#content-purchase').animate({height: 'show'}, 0);
+$('#content-lvl2-purchase').animate({height: 'hide'}, 0);
+$('#content-reciprocal').animate({height: 'show'}, 0);
+$('#content-lvl2-reciprocal').animate({height: 'hide'}, 0);
+$('#content-taxation').animate({height: 'hide'}, 0);
+$('#content-lvl2-taxation').animate({height: 'hide'}, 0);
+$('#content-information').animate({height: 'hide'}, 0);
+$('#content-spouse').animate({height: 'hide'}, 0);
+
