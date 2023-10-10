7d0c9921564e8416278837c609b571ff2cfe141a
Rob Bartholomew <rob@racm.ca>
2023-10-10 10:06:35 -0400

    fixes

--- arieleas-integrated/memberportal-home-setup.js
+++ arieleas-integrated/memberportal-home-setup.js
@@ -21,17 +21,21 @@ $('#progress-bar3').addClass('widget-statusstep-done');
 console.log('Class3b:' + bar3Class);
 
 $('#election-continue-link').on('click', function() {
+    console.log('click');
     var bar3Class = $('#progress-bar3').attr('class');
     if (bar3Class == 'widget-statusstep-notstart') {
+        console.log('notstart');
         $('#progress-bar3').removeClass(bar3Class);
         $('#progress-bar3').addClass('widget-statusstep-inprogress');
     } else {
         if (bar3Class == 'widget-statusstep-inprogress') {
+            console.log('inprogress');
             $('#progress-bar3').removeClass(bar3Class);
             $('#progress-bar3').addClass('widget-statusstep-done');
         }
         else {
             if (bar3Class == 'widget-statusstep-done') {
+                console.log('done');
                 $('#progress-bar3').removeClass(bar3Class);
                 $('#progress-bar3').addClass('widget-statusstep-nostarted');
             }
