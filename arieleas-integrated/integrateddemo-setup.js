72b47ffe04d2346ccdf18a65c88604807beda580
Rob Bartholomew <rob@racm.ca>
2023-10-10 09:30:20 -0400

    Setting up automation on integrated demo

---
+++ arieleas-integrated/integrateddemo-setup.js
@@ -0,0 +1,29 @@
+// This is the initial setup of the demo. Will include instantiating variables to their initial state.
+
+
+
+if (localStorage.getItem('retire-initiate-status') == null ){
+    localStorage.setItem('retire-initiate-status','notstarted');
+}
+
+if (localStorage.getItem('retire-options-status') == null ){
+    localStorage.setItem('retire-options-status','notstarted');
+}
+
+if (localStorage.getItem('retire-insurance-status') == null ){
+    localStorage.setItem('retire-insurance-status','notstarted');
+}
+
+if ( localStorage.getItem('scenOneAge') == null ) {
+    localStorage.setItem('scenOneAge',60);  
+}
+
+if ( localStorage.getItem('scenTwoAge') == null ) {
+    localStorage.setItem('scenTwoAge',60);  
+}
+if ( localStorage.getItem('scenThreeAge') == null ) {
+    localStorage.setItem('scenThreeAge',60);  
+}
+
+localStorage.setItem("pensionFrequency", 12);
+
---
+++ arieleas-integrated/memberportal-home-setup.js
@@ -0,0 +1,15 @@
+
+$( document ).ready(function() {
+    $('#tile-abouttheplan').animate({width: '250px'},0);
+    $('#tile-abouttheplan').animate({minWidth: '250px'},{duration: 0, queue: false});
+    $('#tilelarge-abouttheplan').animate({opacity: 'hide'},0);
+    $('#tilesmall-abouttheplan').animate({width: 'show'},{duration: 0, queue: false});
+
+    $('#tile-retirementinfo').animate({width: '250px'},0);
+    $('#tile-retirementinfo').animate({minWidth: '250px'},{duration: 0, queue: false});
+    $('#tilelarge-retirementinfo').animate({opacity: 'hide'},0);
+    $('#tilesmall-retirementinfo').animate({width: 'show'},{duration: 0, queue: false});
+});
+
+var bar1Class = $('#progress-bar1').attr('class');
+console.log('Class:' + bar1Class);
