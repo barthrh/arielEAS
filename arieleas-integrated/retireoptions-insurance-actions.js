fb5fcf041160935ddcea26aea629731313b8ebf0
Rob Bartholomew <rob@racm.ca>
2023-10-11 07:32:19 -0400

    code for insurance page

---
+++ arieleas-integrated/retireoptions-insurance-actions.js
@@ -0,0 +1,21 @@
+// Start by setting the size and visibility of objects.
+
+var insuranceStatus = localStorage.getItem('insuranceStatus');
+if (insuranceStatus == null){
+    localStorage.setItem('insuranceStatus','notstarted');
+    insuranceStatus = 'notstarted';
+}
+
+if (insuranceStatus == 'notstarted') {
+    $('#frame-insurance-done').animate({height: 'hide'}, 0);
+    $('#frame-insurance-notstarted').animate({height: 'show'}, 0);
+} else {
+    $('#frame-insurance-done').animate({height: 'show'}, 0);
+    $('#frame-insurance-notstarted').animate({height: 'hide'}, 0);
+}
+
+$('#button-insurance-enroll').on('click', function() {
+    // takind the easy path of just calling insurance done when you go to it.
+    localStorage.setItem('insuranceStatus','done');
+    window.open("/integrated/retireonline/retire-choices/retire-choices-election");
+})
