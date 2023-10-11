9753eaf66b6c55c48f283f5d25d8f8980f3d67f5
Rob Bartholomew <rob@racm.ca>
2023-10-11 17:39:03 -0400

    a

--- arieleas-integrated/initretire-myinfo-action.js
+++ arieleas-integrated/initretire-myinfo-action.js
@@ -2,8 +2,8 @@
 console.log('Myinfo-actions loaded.');
 
 $('#button-add-cancel').on('click',function() {
-    $('#add-benef-frame').animate({height: 'hide'}, 0);
-    $('#update-benef-frame').animate({height: 'show'}, 0);
+    console.log('cancel the add');
+    $('#enter-beneficiary-frame').animate({height: 'hide'}, 0);
 })
 
 $('#link-addbenny-pri').on('click',function() {
@@ -16,4 +16,9 @@ $('#link-addbenny-sec').on('click',function() {
     console.log('secondary');
     $('#add-benef-frame').animate({height: 'show'}, 0);
     $('#update-benef-frame').animate({height: 'hide'}, 0);
+})
+
+$('#link-addnew-beneficiary').on('click',function(){
+    console.log('summon the add');
+    $('#enter-beneficiary-frame').animate({height: 'hide'}, 0);
 })
