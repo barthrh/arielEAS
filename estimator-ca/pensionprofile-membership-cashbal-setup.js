036275b022e2602b24186eb391f14fcf48ad0ea1
Rob Bartholomew <rob@racm.ca>
2024-01-22 13:54:04 -0500

    New cashback membership

---
+++ estimator-ca/pensionprofile-membership-cashbal-setup.js
@@ -0,0 +1,6 @@
+// One-time actions at page load
+
+$('#content-retirementdates').animate({height: 'hide'}, 500);
+$('#content-benefits').animate({height: 'show'}, 500);
+$('#content-contributions').animate({height: 'hide'}, 500);
+$('#content-salaries').animate({height: 'hide'}, 500);
