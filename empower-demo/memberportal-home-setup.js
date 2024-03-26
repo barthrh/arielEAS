f2c814318a205ae59626ba1137d7da9e3a3991bb
Rob Bartholomew <rob@racm.ca>
2024-03-26 12:20:08 -0400

    finish tile hiding

--- empower-demo/memberportal-home-setup.js
+++ empower-demo/memberportal-home-setup.js
@@ -1,20 +1,22 @@
-// Test Data
-localStorage.setItem('tile-estimate','show');
-localStorage.setItem('tile-profile','hide');
-
-
-// Queue up the default variables to enable or disable tiles on the home page.
-var showTileEstimate = localStorage.getItem('tile-estimate');
-var showTileProfile = localStorage.getItem('tile-profile');
-var showTileDcpension = localStorage.getItem('tile-dcpension');
-var showTileInitiate = localStorage.getItem('tile-initiate');
 
 
 
 
 $( document ).ready(function() {
 
+    // Test Data
+    localStorage.setItem('tile-estimate','show');
+    localStorage.setItem('tile-profile','hide');
+
+
+    // Queue up the default variables to enable or disable tiles on the home page.
+    var showTileEstimate = localStorage.getItem('tile-estimate');
+    var showTileProfile = localStorage.getItem('tile-profile');
+    var showTileDcpension = localStorage.getItem('tile-dcpension');
+    var showTileInitiate = localStorage.getItem('tile-initiate');
+
     // If these variables were empty, initialize them. The default is to show.
+    // Estimator tile
     if ( showTileEstimate != null ) {
         if (showTileEstimate == 'hide') {
             $('#tile-estimate').hide();
@@ -26,6 +28,7 @@ $( document ).ready(function() {
         $('#tile-estimate').show(); 
     }
     
+    // Profile tile
     if ( showTileProfile != null ) {
         if (showTileProfile == 'hide') {
             $('#tile-profile').hide();
@@ -37,6 +40,29 @@ $( document ).ready(function() {
         $('#tile-profile').show(); 
     }
 
+    // DC Pension tile
+    if ( showTileDcpension!= null ) {
+        if (showTileDcpension == 'hide') {
+            $('#tile-dcpension').hide();
+        } else {
+            $('#tile-dcpension').show(); 
+        } 
+    }
+    else {
+        $('#tile-dcpension').show(); 
+    }
+
+    // Initiate Retirement tile
+    if ( showTileInitiate != null ) {
+        if (showTileInitiate == 'hide') {
+            $('#tile-initiate').hide();
+        } else {
+            $('#tile-initiate').show(); 
+        } 
+    }
+    else {
+        $('#tile-initiate').show(); 
+    }
 
     $('#tile-abouttheplan').animate({width: '250px'},0);
     $('#tile-abouttheplan').animate({minWidth: '250px'},{duration: 0, queue: false});
