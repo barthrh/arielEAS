330e666966c59e41d3f28c732650968401ef3f37
Rob Bartholomew <rob@racm.ca>
2024-08-23 08:59:13 -0400

    class fix

--- empower-demo/plan-sponsor/ps-participant-summary.js
+++ empower-demo/plan-sponsor/ps-participant-summary.js
@@ -9,11 +9,15 @@ $('#tab-statements').on('click', function(){
     $('#tabcontent-workitems').hide();
     $('#tab-statements').removeClass('inactive');
     $('#tab-statements').addClass('active');
+    $('#tab-workitems').removeClass('active');
+    $('#tab-workitems').addClass('inactive');
 });
 
 $('#tab-workitems').on('click', function(){
     $('#tabcontent-statements').hide();
     $('#tabcontent-workitems').show();
-    $('#tab-workitems').removeClass('active');
-    $('#tab-workitems').addClass('inactive');
+    $('#tab-workitems').removeClass('inactive');
+    $('#tab-workitems').addClass('active');
+    $('#tab-statements').removeClass('active');
+    $('#tab-statements').addClass('inactive');
 });
