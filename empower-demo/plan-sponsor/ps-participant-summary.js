f73b7fd9bfe6b0535465fcbaee32d1ad62fa6b2f
Rob Bartholomew <rob@racm.ca>
2024-08-23 08:56:16 -0400

    class changes

--- empower-demo/plan-sponsor/ps-participant-summary.js
+++ empower-demo/plan-sponsor/ps-participant-summary.js
@@ -7,9 +7,13 @@ $( document ).ready(function() {
 $('#tab-statements').on('click', function(){
     $('#tabcontent-statements').show();
     $('#tabcontent-workitems').hide();
+    $('#tab-statements').removeClass('inactive');
+    $('#tab-statements').addClass('active');
 });
 
 $('#tab-workitems').on('click', function(){
     $('#tabcontent-statements').hide();
     $('#tabcontent-workitems').show();
+    $('#tab-workitems').removeClass('active');
+    $('#tab-workitems').addClass('inactive');
 });
