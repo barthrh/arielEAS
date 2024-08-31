2ee06b269e096ebdd1403f95af576960883c7a30
Rob Bartholomew <rob@racm.ca>
2024-08-31 10:32:26 -0400

    Member detail page setup

---
+++ payroll-portal/pr-employer-portal-member.js
@@ -0,0 +1,62 @@
+$( document ).ready(function() {
+    $('#tab-member-content').hide();
+    $('#tab-payment-content').hide();
+    $('#tab-banking-content').hide();
+    $('#tab-tax-content').hide();
+    $('#tab-paysources-content').hide();
+
+    $('#payrow-content-2023').hide();
+    $('#payrow-content-2024').hide();
+
+    console.log( "ready!" );
+});
+
+// All of the tab controls. Don't know where we are coming from (too lazy) so just toggle it all.
+$('#tab-member').on('click', function(){
+    $('#tab-member-content').show();
+    $('#tab-payment-content').hide();
+    $('#tab-banking-content').hide();
+    $('#tab-tax-content').hide();
+    $('#tab-paysources-content').hide();
+ });
+ 
+ $('#tab-payment').on('click', function(){
+    $('#tab-member-content').hide();
+    $('#tab-payment-content').show();
+    $('#tab-banking-content').hide();
+    $('#tab-tax-content').hide();
+    $('#tab-paysources-content').hide();
+ });
+
+ $('#tab-banking').on('click', function(){
+    $('#tab-member-content').hide();
+    $('#tab-payment-content').hide();
+    $('#tab-banking-content').show();
+    $('#tab-tax-content').hide();
+    $('#tab-paysources-content').hide();
+ });
+
+ $('#tab-tax').on('click', function(){
+    $('#tab-member-content').hide();
+    $('#tab-payment-content').hide();
+    $('#tab-banking-content').hide();
+    $('#tab-tax-content').show();
+    $('#tab-paysources-content').hide();
+ });
+
+ $('#tab-paysources').on('click', function(){
+    $('#tab-member-content').hide();
+    $('#tab-payment-content').hide();
+    $('#tab-banking-content').hide();
+    $('#tab-tax-content').hide();
+    $('#tab-paysources-content').show();
+ });
+
+ // year accordians in the pay table
+ $('#payrow-year-2023').on('click',function(){
+    $('#payrow-content-2023').animate({height: 'toggle'}, 500);
+ });
+
+ $('#payrow-year-2024').on('click',function(){
+    $('#payrow-content-2024').animate({height: 'toggle'}, 500);
+ });
