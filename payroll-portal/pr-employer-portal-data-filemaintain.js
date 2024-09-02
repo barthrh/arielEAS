b0c704e0d4b5922b2f7d8c8281ff9413396ef8f6
Rob Bartholomew <rob@racm.ca>
2024-09-02 10:22:57 -0400

    debug

--- payroll-portal/pr-employer-portal-data-filemaintain.js
+++ payroll-portal/pr-employer-portal-data-filemaintain.js
@@ -1,7 +1,14 @@
 $('#multirow-delete').on('click',function(){
-    var multiblock = $(this).closest('prer-dataentry-multirow-block')
+    var multiblock = $(this).closest('prer-dataentry-multirow-block');
     
+    var multiblock2 = $(this).parent().parent();
+
     multiblock.hide();
     multiblock.animate({height: 'show'}, 500);
+
+    multiblock2.hide();
+    multiblock2.animate({height: 'show'}, 500);
+
     console.log(multiblock);
+
 });
