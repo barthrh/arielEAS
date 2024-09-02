e5c239f8836bcd8c3fe7c27c53bf3f0f7fa9add6
Rob Bartholomew <rob@racm.ca>
2024-09-02 11:00:40 -0400

    test

--- payroll-portal/pr-employer-portal-data-filemaintain.js
+++ payroll-portal/pr-employer-portal-data-filemaintain.js
@@ -14,11 +14,21 @@ $('#multirow-add').on('click',function(){
     
     var multiblock1 = $(this).parent();
     var multiblock2 = $(this).parent().parent();
-    var multiblock3 = $(this).parent().parent().next('div#hideblock.prer-dataentry-multirow-block.hide');
+    var multiblock3 = $(this).parent().parent().children('div#hideblock.prer-dataentry-multirow-block.hide');
+    var multiblock4 = $(this).parent().parent().children('div.prer-dataentry-multirow-block.hide');
+    var multiblock5 = $(this).parent().parent().children('.div#hideblock.prer-dataentry-multirow-block.hide');
+    var multiblock6 = $(this).parent().parent().children('.div.prer-dataentry-multirow-block.hide');
+    var multiblock7 = $(this).parent().parent().children('.hide');
+    var multiblock8 = $(this).parent().parent().next('.hide');
 
     console.log(multiblock1);
     console.log(multiblock2);
     console.log(multiblock3);
+    console.log(multiblock4);
+    console.log(multiblock5);
+    console.log(multiblock6);
+    console.log(multiblock7);
+    console.log(multiblock8);
 
 });
 
