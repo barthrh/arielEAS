9dff1e4b7d0ee252039d4ecbbb2440fd9d3c4b65
Rob Bartholomew <rob@racm.ca>
2024-09-03 09:15:18 -0400

    accordian

---
+++ payroll-portal/pr-member-portal-profile-payment.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+    $('#payrow-content-2024').show();
+    $('#payrow-content-2023').hide();
+    $('#payrow-content-2022').hide();
+    $('#payrow-content-2021').hide();
+
+    console.log( "ready!" );
+});
+
+
+$('#accordian-pay2024').on('click',function(){
+    $('#payrow-content-2024').animate({height: 'toggle'}, 500);
+    $('#payrow-content-2024').rotateImage(180);
+ });
+
+ $('#accordian-pay2023').on('click',function(){
+    $('#payrow-content-2023').animate({height: 'toggle'}, 500);
+ });
+
+ $('#accordian-pay2022').on('click',function(){
+    $('#payrow-content-2022').animate({height: 'toggle'}, 500);
+ });
+
+ $('#accordian-pay2021').on('click',function(){
+    $('#payrow-content-2021').animate({height: 'toggle'}, 500);
+ });
+
+ function rotateImage(degree) {
+	$('#demo-image').animate({  transform: degree }, {
+    step: function(now,fx) {
+        $(this).css({
+            '-webkit-transform':'rotate('+now+'deg)', 
+            '-moz-transform':'rotate('+now+'deg)',
+            'transform':'rotate('+now+'deg)'
+        });
+    }
+    });
+}
