b89424117b0b69b8b5a394d040b72a8bcac30509
Rob Bartholomew <rob@racm.ca>
2024-10-15 13:57:29 -0400

    more values

--- ariel-canada/profile-active-personal.js
+++ ariel-canada/profile-active-personal.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
 
     $('#res-addr-line1').val('123 Main St.');
+    $('#res-addr-line2').val('');
+    $('#res-addr-city').val('Demoville');
+    $('#res-addr-postal').val('M9K 2L3');
+    $('#res-addr-prov').val('Ontario');
+    $('#res-addr-country').val('Canada');
 
 });
