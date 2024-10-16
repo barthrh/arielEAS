dde07ceac0212d16771c1eb015df6a877991f71d
Rob Bartholomew <rob@racm.ca>
2024-10-16 15:17:20 -0400

    New

---
+++ empower-demo/profile-retiree-personal.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+    $('#res-addr-line1').val('123 Main St.');
+    $('#res-addr-line2').val('');
+    $('#res-addr-city').val('Demoville');
+    $('#res-addr-postal').val('43234');
+    $('#res-addr-prov').val('OH');
+    $('#res-addr-country').val('US');
+    $('#res-addr-email').val('mark.j.thompson@empowerdemo.com');
+    $('#res-addr-mobile').val('330-555-8283');
+    $('#res-addr-phone').val('330-555-3823');
+});
