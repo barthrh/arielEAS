04dd59a288129a00779471d29e90649d5aecddb2
Rob Bartholomew <rob@racm.ca>
2024-10-21 17:27:42 -0400

    more

--- empower-demo/plan-sponsor/ps-reports-ppsummary.js
+++ empower-demo/plan-sponsor/ps-reports-ppsummary.js
@@ -6,12 +6,30 @@ $( document ).ready(function() {
 
 });
 
-$('#param-select-month').on('click', function(){
+$('#radio-month').on('click', function(){
     if ($(this).prop("checked") == true) {
        $('#param-select-month').show();
        $('#param-select-year').hide();
        $('#param-select-period').hide();
+
+       $('#radio-year').prop("checked",false);
+       $('#radio-period').prop("checked",false);
+       $('#radio-all').prop("checked",false);
     } else {
         $('#param-select-month').hide();
+    }
+ });
+
+ $('#radio-year').on('click', function(){
+    if ($(this).prop("checked") == true) {
+       $('#param-select-month').hide();
+       $('#param-select-year').show();
+       $('#param-select-period').hide();
+
+       $('#radio-month').prop("checked",false);
+       $('#radio-period').prop("checked",false);
+       $('#radio-all').prop("checked",false);
+    } else {
+        $('#param-select-year').hide();
     }
  });
