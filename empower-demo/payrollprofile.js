49b2930e49e6de6680252b17bb66fedd3eb30da9
Rob Bartholomew <rob@racm.ca>
2024-10-23 22:32:05 -0400

    new

---
+++ empower-demo/payrollprofile.js
@@ -0,0 +1,15 @@
+var addrLine1 = '123 Main St.';
+var addrLine2 = '';
+var addrCity = 'Demoville';
+var addrState = 'OH';
+var addrZip = '43027';
+var addrCountry = 'United States';
+
+var maritalStatus = 'married';
+var gender = 'male';
+var emailAddr = 'tommy.j.thompson@telusdemo.com';
+var phone = '330-555-3823';
+var mobile = '330-555-8283';
+
+console.log($('#resident-address').val());
+
