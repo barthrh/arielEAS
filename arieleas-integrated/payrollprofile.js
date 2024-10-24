19c965d6d6b11f1d9eb8bfb30ddf203ddd46b416
Rob Bartholomew <rob@racm.ca>
2024-10-24 14:27:08 -0400

    change values

--- arieleas-integrated/payrollprofile.js
+++ arieleas-integrated/payrollprofile.js
@@ -1,15 +1,15 @@
 var addrLine1 = '1234 Main St.';
 var addrLine2 = '';
 var addrCity = 'Demotown';
-var addrState = 'OH';
-var addrZip = '43028';
+var addrState = 'MA';
+var addrZip = '01082';
 var addrCountry = 'United States';
 
 var maritalStatus = 'married';
 var gender = 'male';
-var emailAddr = 'tommy.j.thompson@telusdemo.com';
-var phone = '330-555-3823';
-var mobile = '330-555-8283';
+var emailAddr = 'joseph.r.truman@telusdemo.com';
+var phone = '351-555-3823';
+var mobile = '351-555-8283';
 
 console.log($('#resident-address').text());
 
