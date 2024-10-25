d99f5e4692225cdb6b9e7ecd3b030cf21b208e27
Rob Bartholomew <rob@racm.ca>
2024-10-25 11:52:39 -0400

    suppress form

--- payroll-portal/pr-employer-portal-dashboard.js
+++ payroll-portal/pr-employer-portal-dashboard.js
@@ -4,6 +4,12 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
+// Disable the enter key on forms
+$('#form-find').submit(function() {
+    return false;
+});
+
+
 $('#accordian-report-reveal').on('click', function(){
     $('#accordian-report-content').animate({height: 'toggle'}, 500);
  });
