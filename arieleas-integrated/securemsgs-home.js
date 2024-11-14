f015ed5997b127fd6c21d0d2618fcf3c330a8a17
Rob Bartholomew <rob@racm.ca>
2024-11-13 20:43:59 -0500

    nbew

---
+++ arieleas-integrated/securemsgs-home.js
@@ -0,0 +1,25 @@
+// Actions for the various buttons and links.
+
+$( document ).ready(function() {
+    
+    $('#menurow-archive').hide();
+    $('#menuselect-archive').hide();
+    
+    console.log( "ready!" );
+});
+
+$('#menurow-inbox').on('click', function(){
+    $('#menurow-inbox').addClass('active');
+    $('#menuselect-inbox').show();
+    $('#menurow-archive').removeClass('active');
+    $('#menuselect-archive').hide();
+
+})
+
+$('#menurow-archive').on('click', function(){
+    $('#menurow-inbox').removeClass('active');
+    $('#menuselect-inbox').hide();
+    $('#menurow-archive').addClass('active');
+    $('#menuselect-archive').show();
+
+})
