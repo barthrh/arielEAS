d47e7a1fdc4c76edd6a9a913c531f153edac4c91
Rob Bartholomew <rob@racm.ca>
2025-05-27 15:02:53 -0400

    Beneficiary form

--- estimator-ca/pensionprofile-personal-actions.js
+++ estimator-ca/pensionprofile-personal-actions.js
@@ -1,5 +1,13 @@
 // Code to hide or show the menus and content panes
 
+$( document ).ready(function() {
+    
+    $('#add-benef-frame').animate({height: 'hide'}, 0);
+    $('#update-benef-frame').animate({height: 'show'}, 0);
+    
+    console.log( "ready!" );
+});
+
 $('#nav-basic-tab').on('click', function(){
     setBasic('show');
     setSurvivor('hide');
@@ -59,3 +67,51 @@ function setAuthrep(theAction) {
     $('#nav-authrep-content').animate({height: showHide}, 300);
 }
 // nav-authrep-tab
+
+
+// Beneficiary Form support
+
+$('#button-add-cancel').on('click',function() {
+    console.log('cancel the add');
+    $('#enter-beneficiary-frame').animate({height: 'hide'}, 100);
+    $('#button-add-cancel').animate({height: 'hide'}, 0);
+    $('#button-add-done').text('Done');
+})
+
+$('#link-addbenny-pri').on('click',function() {
+    console.log('primary');
+    $('#add-benef-frame').animate({height: 'show'}, 0);
+    $('#update-benef-frame').animate({height: 'hide'}, 0);
+    $('#button-add-cancel').animate({height: 'hide'}, 0);
+    $('#button-add-done').text('Done');
+})
+
+$('#link-addbenny-sec').on('click',function() {
+    console.log('secondary');
+    $('#add-benef-frame').animate({height: 'show'}, 0);
+    $('#update-benef-frame').animate({height: 'hide'}, 0);
+    $('#button-add-cancel').animate({height: 'hide'}, 0);
+    $('#button-add-done').text('Done');
+})
+
+$('#link-addnew-beneficiary').on('click',function(){
+    console.log('summon the add');
+    $('#enter-beneficiary-frame').animate({height: 'show'}, 100);
+    $('#button-add-cancel').animate({height: 'show'}, 0);
+    $('#button-add-done').text('Add');
+})
+
+$('#close-addbenny').on('click',function(){
+    console.log('Close add');
+    $('#add-benef-frame').animate({height: 'hide'}, 0);
+    $('#update-benef-frame').animate({height: 'show'}, 0);
+})
+
+$('#button-add-done').on('click',function() {
+    console.log('Done event');
+    if ($('#button-add-done').text = 'Done') {
+        console.log('Done pressed');
+        $('#add-benef-frame').animate({height: 'hide'}, 0);
+        $('#update-benef-frame').animate({height: 'show'}, 0);
+    }
+})
