e9ec208a964e8fb32db93cc17b8b6965182a675e
Rob Bartholomew <rob@racm.ca>
2025-08-06 22:29:59 -0400

    tst

--- empower-demo/empower-landing.js
+++ empower-demo/empower-landing.js
@@ -8,16 +8,19 @@ $(document).ready(function () {
   // Start fresh to clear the cache
   localStorage.removeItem('savedImageBase64');
 
+  let logoBase64 = '';
+
   // Grab a freshly posted logo
   $.get('https://empower-manager-514504a17a0e.herokuapp.com/getLogoBase64', function(data) {
     localStorage.setItem('savedImageBase64', data);
+    logoBase64 = data;
     console.log('Base64 logo stored.');
   }).fail(function() {
     console.log('No Base64 logo found.');
   });
 
   // Set the landing image, just for now
-  let logoBase64 = localStorage.getItem(('savedImageBase64'));
+  // let logoBase64 = localStorage.getItem(('savedImageBase64'));
   console.log('TEST: ' + localStorage.getItem(('savedImageBase64')));
   console.log('LOGO: ' + 'HERE: ' + logoBase64);
 
