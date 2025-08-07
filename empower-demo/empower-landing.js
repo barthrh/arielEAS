24b46fff874db288a6a08244d1a3406d92bcf881
Rob Bartholomew <rob@racm.ca>
2025-08-06 21:45:19 -0400

    fix

--- empower-demo/empower-landing.js
+++ empower-demo/empower-landing.js
@@ -18,6 +18,7 @@ $(document).ready(function () {
 
   // Set the landing image, just for now
   let logoBase64 = localStorage.getItem(('savedImageBase64'));
+  console.log(logoBase64);
 
   // Set the image source
   $('#portalLogo').attr('src', logoBase64);
