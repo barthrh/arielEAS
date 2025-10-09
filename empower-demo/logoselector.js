823d4d165fa7d609f4592112999af7e10cb08e54
Rob Bartholomew <rob@racm.ca>
2025-10-09 11:29:39 -0400

    Made function async due to sub finishing too late

--- empower-demo/logoselector.js
+++ empower-demo/logoselector.js
@@ -14,9 +14,9 @@ for serviceModel, I'm anticipating: PREMIUM, STANDARD
 */
 
 
-$('#but-openmember').on('click',function() {
+$('#but-openmember').on('click', async function() {
 
-  let targetURL = selectLogo();
+  let targetURL = await selectLogo();
 
   console.log('Received member target:' + targetURL);
 
@@ -24,9 +24,9 @@ $('#but-openmember').on('click',function() {
   window.open(targetURL,'_blank');
 });
 
-$('#but-opensponsor').on('click',function() {
+$('#but-opensponsor').on('click', async function() {
 
-  let targetURL = selectLogo();
+  let targetURL = await selectLogo();
 
   console.log('Received sponsor target:' + targetURL);
 
@@ -37,50 +37,53 @@ $('#but-opensponsor').on('click',function() {
 
 function selectLogo() {
 
-  $('#logoSelectError').hide();
+  return new Promise((resolve, reject) => {
 
-  // Get selected radio button value
-  let radioValue = $('input[name="radio-logoSelect"]:checked').val();
-  if (radioValue != null) {
-    console.log('radio val' + radioValue);
+    $('#logoSelectError').hide();
 
-    let base64ImageString = $('#' + radioValue).text();
-    console.log('base64 val: ' + base64ImageString);
+    // Get selected radio button value
+    let radioValue = $('input[name="radio-logoSelect"]:checked').val();
+    if (radioValue != null) {
+      console.log('radio val' + radioValue);
 
-    let choiceDbPlanType = $('#dbPlanType').val();
-    let choiceServiceModel = $('#serviceModel').val();
+      let base64ImageString = $('#' + radioValue).text();
+      console.log('base64 val: ' + base64ImageString);
+
+      let choiceDbPlanType = $('#dbPlanType').val();
+      let choiceServiceModel = $('#serviceModel').val();
 
 
-    // Post the logo to the Node server
-    $.ajax({
-      url: 'https://empower-manager-514504a17a0e.herokuapp.com/save-image',
-      type: 'POST',
-      contentType: 'application/json',
-      data: JSON.stringify({ dbPlanType: choiceDbPlanType, serviceModel: choiceServiceModel, base64Image: base64ImageString }),
-      success: function(response) {
+      // Post the logo to the Node server
+      $.ajax({
+        url: 'https://empower-manager-514504a17a0e.herokuapp.com/save-image',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ dbPlanType: choiceDbPlanType, serviceModel: choiceServiceModel, base64Image: base64ImageString }),
+        success: function(response) {
 
-        // Use the URL in the JSON response to redirect to the proper page
-        console.log('Upload successful:', response);
+          // Use the URL in the JSON response to redirect to the proper page
+          console.log('Upload successful:', response);
 
-        let dbPlanType = response.dbPlanType;
-        let serviceModel = response.serviceModel;
-        let targetURL = response.destinationURL;
+          let dbPlanType = response.dbPlanType;
+          let serviceModel = response.serviceModel;
+          let targetURL = response.destinationURL;
 
-        console.log('Type: ' + dbPlanType + '; model: ' + serviceModel);
-        console.log('Target: ' + targetURL);
+          console.log('Type: ' + dbPlanType + '; model: ' + serviceModel);
+          console.log('Target: ' + targetURL);
 
-        return targetURL;
-        
-      },
-      error: function(xhr, status, error) {
-        console.error('Upload failed:', error);
-        return '';
-      }
-    });
+          return targetURL;
+          
+        },
+        error: function(xhr, status, error) {
+          console.error('Upload failed:', error);
+          return '';
+        }
+      });
 
-  } else {
-    console.log('no radio selected');
-    $('#logoSelectError').show();
-    return '';
-  }
+    } else {
+      console.log('no radio selected');
+      $('#logoSelectError').show();
+      return '';
+    }
+  });
 };
