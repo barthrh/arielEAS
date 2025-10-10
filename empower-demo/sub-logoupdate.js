1783e60588ce1d7415ff1f4c1ae9bdd7646bc2de
Rob Bartholomew <rob@racm.ca>
2025-10-10 11:46:40 -0400

    removed srcset attribute

--- empower-demo/sub-logoupdate.js
+++ empower-demo/sub-logoupdate.js
@@ -27,6 +27,7 @@ function updateLogo() {
   if (logoBase64 && logoBase64 !== '') {
     if (logoBase64.length > 100) {
       $('#portalLogo').attr('src', logoBase64);
+      $('#portalLogo').removeAttr('srcset');
       console.log('Logo updated');
     } else {
       console.log('Short <100 Logo');
