f7e30b855305448072be4c08ff75085d6e979dc4
Rob Bartholomew <rob@racm.ca>
2025-10-22 18:01:48 -0400

    dynamic date end employ

--- initate-retirement/initretire-endofemploy-setup.js
+++ initate-retirement/initretire-endofemploy-setup.js
@@ -3,13 +3,19 @@
 var endDate = localStorage.getItem('initretire-enddate');
 var retireDate = localStorage.getItem('initretire-retiredate');
 
+const date = new Date(); // Sets to current
 
-$("#endemploy-date-input-field").val("12-12-2022");
+let today = formatDate(date);
+let calcRetireDate = calcRetireDate(today);
+let calcEndDate = retireDate - 1;
+
+console.log('calcEndDate: ' + calcEndDate);
 
 if ( localStorage.getItem('initretire-enddate') != null ) {
    $("#endemploy-date").val(endDate);
 } else {
-   $("#endemploy-date").val("09-30-2023");  
+   // $("#endemploy-date").val("09-30-2023");  
+   $('#endemploy-date').val(formatDate(date));
 }
 
 if ( localStorage.getItem('initretire-retiredate') != null ) {
@@ -19,4 +25,14 @@ if ( localStorage.getItem('initretire-retiredate') != null ) {
    $("#retirement-date").text("October 1, 2023")  
    console.log("Point4");
 }
+
+
+// This is only for the OUTPUTS page sets the date to recent
+function formatDate(date) {
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+  
+  return `${month}-${day}-${year}`;
+};
 
